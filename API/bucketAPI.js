ec624156e67c9cbb04bb8f961c944c81637c676a
Savin Volodya <SavinVolodya001@gmail.com>
2021-08-22 15:44:03 +0300

    feat: add get upload URL lambda

--- API/bucketAPI.js
+++ API/bucketAPI.js
@@ -1,4 +1,6 @@
 const s3Client = require('../serviceModules/S3bucketClient')
+const getAllComments = require('./comments').getAllComments;
+const uuid = require('uuid');
 
 module.exports.upload = async req => {
     let body = JSON.parse(req.body);
@@ -54,4 +56,42 @@ module.exports.download = async req => {
     }
 
     return response;
+}
+
+module.exports.getUploadUrl = async req => {
+    let body = JSON.parse(req.body);
+
+    let response = {
+        statusCode: 200,
+        body: null
+    }
+
+    try{
+        let allComments = await getAllComments({
+            body: JSON.stringify({
+                TableName: process.env.TABLE_NAME
+            })
+        });
+        allComments = JSON.parse(allComments.body)
+        
+        //if there is at least one element with specified ID
+        if(!allComments.some(item => item.comment_id === body.commentID))
+            throw new Error("Item doesn't exist")
+
+
+        response.body = JSON.stringify(await s3Client.getSignedUrlPromise('putObject', {
+            Bucket: process.env.BUCKET_NAME,
+            Key: `comment-images/${body.commentID}/${uuid.v1()}`,
+            Expires: 120, //[s],
+            ContentType: 'image/jpeg'
+        }));
+    }catch(e){
+        response.body = JSON.stringify({
+            errorMessage: e.message
+        })
+
+        response.statusCode = 500;
+    }
+
+    return response
 }
